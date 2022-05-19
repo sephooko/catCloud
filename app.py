e1125967a404b435fd400e6536cea6c47a0832f3
Malwina <yellowindmills@gmail.com>
2022-05-19 23:02:25 +0200

    forward

--- app.py
+++ app.py
@@ -1,29 +1,9 @@
-import os
-
 from flask import Flask, abort, make_response
 from flask import render_template
 from flask_mail import Mail, Message
 from flask import url_for
 
 app = Flask(__name__, template_folder='templates', static_url_path='', static_folder='static')
-mail = Mail(app)
-app.config['MAIL_SERVER'] = 'smtp.mailtrap.io'
-app.config['MAIL_PORT'] = 2525
-app.config['MAIL_USERNAME'] = '6b4e162fafe974'
-app.config['MAIL_PASSWORD'] = 'bbca71a7fc7f07'
-# app.config['MAIL_USERNAME'] = os.environ.get('MAIL_USERNAME')
-# app.config['MAIL_PASSWORD'] = os.environ.get('MAIL_PASSWORD')
-app.config['MAIL_USE_TLS'] = True
-app.config['MAIL_USE_SSL'] = False
-
-
-@app.route('/')
-def email():
-    msg = Message('Hello from the other side!', sender='input@mailtrap.io', recipients=['me@mailtrap.io'])
-    msg.body = "Hey Paul, sending you this email from my Flask app, lmk if it works"
-    msg = Message('Hello from the other side!', sender=("Peter from Mailtrap", 'peter@mailtrap.io'))
-    mail.send(msg)
-    return "Message sent!"
 
 
 @app.route('/index')
--- app2.py
+++ app2.py
@@ -1,24 +1,52 @@
-from flask import Flask
+import email
+
+from flask import Flask, render_template, request, redirect, url_for, flash
 from flask_mail import Mail, Message
 
 app = Flask(__name__)
 
-app.config['MAIL_SERVER']='smtp.mailtrap.io'
+app.config['MAIL_SERVER'] = 'smtp.mailtrap.io'
 app.config['MAIL_PORT'] = 2525
 app.config['MAIL_USERNAME'] = '6b4e162fafe974'
 app.config['MAIL_PASSWORD'] = 'bbca71a7fc7f07'
 app.config['MAIL_USE_TLS'] = True
 app.config['MAIL_USE_SSL'] = False
 
-
 mail = Mail(app)
 
 
+@app.route("/homepage")
 @app.route("/")
 def index2():
-    msg = Message('Hello from the other side!', sender='peter@mailtrap.io', recipients=['paul@mailtrap.io'])
-    msg.body = "Hey Paul, sending you this email from my Flask app, lmk if it works"
-    mail.send(msg)
+    return render_template("index2.html")
+
+
+# @app.route('/create/', methods=('GET', 'POST'))
+# def create():
+#     if request.method == 'POST':
+#         email = request.form['email']
+#         msgText = request.form['msgtext']
+#         if not email:
+#             flash('Email is required!')
+#         elif not msgText:
+#             flash('Content is required!')
+#         else:
+#             return redirect(url_for(index2))
+#     render_template()
+
+
+@app.route("/msgsent", methods=('GET', 'POST'))
+def msgSent():
+    if request.method == 'POST':
+        msg = Message('Hello from the other side!', sender=request.form.get('email'), recipients=['mischief@mailtrap.io'])
+        msg.body = request.form.get('msgtext')
+        mail.send(msg)
+    elif request.form.get('email') is None:
+        flash('Email is required!')
+    elif request.form.get('msgtext') is None:
+        flash('Content is required!')
+    else:
+        return redirect(url_for('index2'))
     return "Message sent!"
 
 
